09f21eb21c8d3cec12c0989538a3bb8ad6e245be
MOONWUD <moonqazwsx@naver.com>
2024-06-12 14:46:37 +0900

    240612 마무리

--- assets/Js/index.js
+++ assets/Js/index.js
@@ -35,13 +35,13 @@ let sections = gsap.utils.toArray(".box");
 
 function goToSection(i) {
   gsap.to(window, {
-    scrollTo: { y: i * innerHeight, autoKill: false, ease: "easeInOut" },
+    scrollTo: { y: i * innerHeight, autoKill: false, ease: "easeIn" },
     duration: 0.9,
   });
 }
 
 ScrollTrigger.defaults({
-  // markers: true
+   //markers: true
 });
 
 sections.forEach((eachPanel, i) => {
@@ -58,6 +58,7 @@ sections.forEach((eachPanel, i) => {
     onEnterBack: () => goToSection(i)
   });
 });
+
 
 
 /*===== header */
@@ -200,89 +201,16 @@ $(window).on('scroll', () => {
 /*===== footer */
 /*=== title */
 
-// class TextScramble {
-//   constructor(el) {
-//     this.el = el
-//     this.chars = `$@&%*(@&()*_________`
-//     this.update = this.update.bind(this)
-//   }
-//   setText(newText) {
-//     const oldText = this.el.innerText
-//     const length = Math.max(oldText.length, newText.length)
-//     const promise = new Promise((resolve) => this.resolve = resolve)
-//     this.queue = []
-//     for (let i = 0; i < length; i++) {
-//       const from = oldText[i] || ''
-//       const to = newText[i] || ''
-//       const start = Math.floor(Math.random() * 40)
-//       const end = start + Math.floor(Math.random() * 40)
-//       this.queue.push({ from, to, start, end })
-//     }
-//     cancelAnimationFrame(this.frameRequest)
-//     this.frame = 0
-//     this.update()
-//     return promise
-//   }
-//   update() {
-//     let output = ''
-//     let complete = 0
-//     for (let i = 0, n = this.queue.length; i < n; i++) {
-//       let { from, to, start, end, char } = this.queue[i]
-//       if (this.frame >= end) {
-//         complete++
-//         output += to
-//       } else if (this.frame >= start) {
-//         if (!char || Math.random() < 0.28) {
-//           char = this.randomChar()
-//           this.queue[i].char = char
-//         }
-//         output += `<span class="dud">${char}</span>`
-//       } else {
-//         output += from
-//       }
-//     }
-//     this.el.innerHTML = output
-//     if (complete === this.queue.length) {
-//       this.resolve()
-//     } else {
-//       this.frameRequest = requestAnimationFrame(this.update)
-//       this.frame++
-//     }
-//   }
-//   randomChar() {
-//     return this.chars[Math.floor(Math.random() * this.chars.length)]
-//   }
-// }
+const content = "LIGNE - ROSET";
+const text = document.querySelector(".footer .title .text");
+let i = 0;
 
-// const phrases = [
-//   'Ligne Roset',
-//   'french furniture manufacturer',
-// ]
-
-// const el = document.querySelector('.footer .title')
-// const fx = new TextScramble(el)
-
-// let counter = 0
-// const next = () => {
-//   fx.setText(phrases[counter]).then(() => {
-//     setTimeout(next, 2500)
-//   })
-//   counter = (counter + 1) % phrases.length
-// }
-
-// next()
-
-
-// const content = "LIGNE - ROSET";
-// const text = document.querySelector(".footer .title .text");
-// let i = 0;
-
-// function typing(){
-//     let txt = content[i++];
-//     text.innerHTML += txt;
-//     if (i > content.length) {
-//         text.textContent = "";
-//         i = 0;
-//     }
-// }
-// setInterval(typing, 300)
+function typing(){
+    let txt = content[i++];
+    text.innerHTML += txt;
+    if (i > content.length) {
+        text.textContent = "";
+        i = 0;
+    }
+}
+setInterval(typing, 300)
